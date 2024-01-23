cb9b5d5e4e4c7f15deab2c8697a4fd476a0c57a6
dellabeneta <m.dellabeneta@gmail.com>
2024-01-23 20:27:45 -0300

    .

--- registry.tf
+++ registry.tf
@@ -1,4 +1,4 @@
 resource "digitalocean_container_registry" "k8s-registry" {
   name                   = var.registry_name
-  subscription_tier_slug = "starter"
+  subscription_tier_slug = "basic"
 }
